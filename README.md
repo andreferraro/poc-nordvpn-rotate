248f1dcb6a4a265eb478ab6b672f17471a9673b5
Andre Ferraro <aferrarobr@gmail.com>
2022-07-26 15:19:36 -0300

    feat: add endpoints ip, connect, disconnect and rotate

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 
 POC que implementa endpoints para rotacionar o IP da máquina usando NordVPN.
 A rotação do IP deverá ser dentro de uma imagem Docker.
+
+## Usage
+
+### Local
+
+```bash
+curl -X GET http://localhost:5000/ip
+```
---
+++ __main__.py
@@ -0,0 +1,41 @@
+"""
+POC Sidecar
+"""
+import urllib.request
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+
+@app.route("/ip", methods=["GET"])
+def my_ip():
+    external_ip = urllib.request.urlopen(
+        "https://ident.me").read().decode("utf8")
+    if request.method == "GET":
+        data = {"ip": external_ip}
+        return jsonify(data)
+
+
+@app.route("/connect", methods=["GET"])
+def connect():
+    if request.method == "GET":
+        data = {"status": True}
+        return jsonify(data)
+
+
+@app.route("/rotate", methods=["GET"])
+def rotate():
+    if request.method == "GET":
+        data = {"status": True}
+        return jsonify(data)
+
+
+@app.route("/disconnect", methods=["GET"])
+def disconnect():
+    if request.method == "GET":
+        data = {"status": True}
+        return jsonify(data)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
